ba3d662b4181f16a6f21fa4be064da6e6862c79c
Christos <christos@b89.io>
2021-08-01 18:57:42 -0500

    feature: convertion of 7 banks

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import { IBankDetails } from './interfaces/BankDetails';
+import BankCCIArray from './constants/BankCCI.json';
+
+const getBank = (bankIdentifier: string) => {
+  return BankCCIArray.find((BankCCI) =>  BankCCI.id === bankIdentifier)?.name;
+}
+
+export const getDetailsFromCCI = (cci: string): IBankDetails => {
+  if (!cci.match(/[0-9]{20}/)) {
+    throw new Error('CCI must be numeric and have exactly 20 digits');
+  }
+
+  const bankIdentifier = cci.substring(0, 3);
+  const bank = getBank(bankIdentifier);
+
+  if (!bank) {
+    throw new Error("Unknown Bank Identifier");
+  }
+
+  switch (bankIdentifier) {
+    case "BCP":
+      const accountTypeDigit = cci.substring(6, 7);
+      if (accountTypeDigit === '1') {
+        // Ahorro
+        return {
+          bank,
+          accountNumber: cci.substring(3, 6) + cci.substring(7, 18),
+          cci,
+          currency: cci.substring(15, 16) === '0' ? 'PEN' : 'USD',
+          type: 'Ahorro',
+        };
+      } else {
+        // Cuenta Corriente
+        return {
+          bank,
+          accountNumber: cci.substring(3, 6) + cci.substring(8, 18),
+          cci,
+          currency: cci.substring(15, 16) === '0' ? 'PEN' : 'USD',
+          type: 'Corriente',
+        };
+      }
+    case "INTERBANK":
+      return {
+        bank,
+        accountNumber: cci.substring(3, 6) + cci.substring(8, 18),
+        cci,
+      };
+    case "BBVA":
+      return {
+        bank,
+        accountNumber: '0' + '011' + '0' + cci.substring(3, 6) + cci.substring(8, 18),
+        cci,
+      };
+    case "SCOTIABANK":
+      return {
+        bank,
+        accountNumber: cci.substring(3, 6) + cci.substring(11, 18),
+        cci,
+      };
+    case "BANBIF":
+      return {
+        bank,
+        accountNumber: '0' + cci.substring(7, 18),
+        cci,
+      };
+    case "MIBANCO":
+      return {
+        bank,
+        accountNumber: cci.substring(8, 18),
+        cci,
+      };
+    case 'BANCO DE LA NACION':
+      return {
+        bank,
+        accountNumber: cci.substring(7, 18),
+        cci,
+      };
+    default:
+      throw new Error('Unknown Bank Identifier');
+  }
+};
---
+++ src/interfaces/BankDetails.ts
@@ -0,0 +1,7 @@
+export interface IBankDetails {
+  bank: string;
+  accountNumber: string;
+  cci: string;
+  currency?: string;
+  type?: string;
+}
